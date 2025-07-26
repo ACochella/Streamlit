ad29cd220b839dbde619aa71949aa3f965a620a7
ACochella <56398217+ACochella@users.noreply.github.com>
2025-07-26 20:37:07 -0300

    Add files via upload

--- streamlit.py
+++ streamlit.py
@@ -27,8 +27,3 @@ st.write('Seleccione los filtros para ver los jugadores que cumplan ese criterio
 st.session_state.position = st.selectbox("Elige tu opción:", mapeo["position"].unique().tolist(),index=None)
 
 st.session_state.role = st.selectbox("Elige tu opción:", mapeo[mapeo["position"] == st.session_state.position].role.unique().tolist(),index=None)
-
-if st.session_state.role == "Extremo":
-    st.write("No sos puto")
-else:
-    st.write("Sos puto")
