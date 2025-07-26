b782e6e6efc9e169c228ee343de58433a37348bc
ACochella <56398217+ACochella@users.noreply.github.com>
2025-07-26 20:41:14 -0300

    Add files via upload

--- streamlit.py
+++ streamlit.py
@@ -27,3 +27,10 @@ st.write('Seleccione los filtros para ver los jugadores que cumplan ese criterio
 st.session_state.position = st.selectbox("Elige tu opción:", mapeo["position"].unique().tolist(),index=None)
 
 st.session_state.role = st.selectbox("Elige tu opción:", mapeo[mapeo["position"] == st.session_state.position].role.unique().tolist(),index=None)
+
+stats_players = pd.read_csv('/data/prd/stats.csv', encoding='utf-8')
+
+st.write(st.session_state.position)
+
+stats_role = stats_players[stats_players["classification"] == st.session_state.position + st.session_state.role]
+st.write(stats_role)
