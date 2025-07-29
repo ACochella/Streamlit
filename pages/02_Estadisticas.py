6a41d6eb05f3d3fa2aba605a015fce5c92f3fe0a
ACochella <56398217+ACochella@users.noreply.github.com>
2025-07-29 19:35:23 -0300

    Add files via upload

--- pages/02_Estadisticas.py
+++ pages/02_Estadisticas.py
@@ -120,8 +120,8 @@ with col1:
                 ax.scatter(x=0, y=valores[metrica], color='red', zorder=5)
                 ax.legend()
 
+            fig.subplots_adjust(top=0.93)
             fig.suptitle("Rendimiento del jugador", fontsize=16)
-            plt.tight_layout(rect=[0, 0, 1, 0.95])
             st.pyplot(fig)
         else:
             st.info("Este jugador no supera el percentil 80 en ninguna métrica.")
