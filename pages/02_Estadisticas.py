7150be955f2b7e296709540677d019b720e08b6e
ACochella <56398217+ACochella@users.noreply.github.com>
2025-07-29 00:20:51 -0300

    Add files via upload

--- pages/02_Estadisticas.py
+++ pages/02_Estadisticas.py
@@ -94,6 +94,9 @@ jugador = st.selectbox("Seleccione un jugador", stats_role_tmp["player"].unique(
 
 percentiles = stats_role_tmp.drop(["team","nation","age","MP","starts","minutes","market_value_millions"], axis=1).set_index('player')
 percentiles = percentiles.rank(pct=True).multiply(100).round(1)
+st.write(percentiles)
+valores = percentiles.loc[jugador]
+st.write(valores)
 
 col1, col2 = st.columns(2) # Esto crea dos columnas de igual ancho
 
